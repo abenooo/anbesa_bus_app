d8a7fc30c993ef48860f30f0fc955683ac910cb2
Abenezer <abenezerkifle000@gmail.com>
2025-01-03 07:44:54 -0800

    Update the ios setting

--- lib/services/bus_route_service.dart
+++ lib/services/bus_route_service.dart
@@ -6,9 +6,9 @@ class BusRouteService {
   static Future<List<BusRoute>> getBusRoutes() async {
     try {
       final String response = await rootBundle.loadString('assets/bus_routes.json');
-      print('Loaded JSON: $response'); // Debug print
+      // print('Loaded JSON: $response'); // Debug print
       final List<dynamic> data = json.decode(response) as List<dynamic>;
-      print('Decoded JSON: $data'); // Debug print
+      // print('Decoded JSON: $data'); // Debug print
       return data
           .map((json) => BusRoute.fromJson(json as Map<String, dynamic>))
           .toList();
